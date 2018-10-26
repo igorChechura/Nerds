cc0548d8d7b44a8fb43f9a007dc136122a2c596b
Igor Chechura <igorchechura@gmail.com>
2018-10-26 14:44:55 +0300

    Валидация формы обратной связи

--- js/main.js
+++ js/main.js
@@ -29,45 +29,74 @@ showSlider();
 /* modal contact us */
 
 function toggleModal() {
-	
+
+	var contactUsLink = document.querySelector('.contact-us-link');
+	var modalContact = document.querySelector('.modal-contact');
+	var modalOverlay = document.querySelector('.modal-overlay');
+	var modalClose = document.querySelector('.modal-close');
+
+	var form = document.querySelector('.contact-form');
+	var inputName = document.querySelector('[name=name]');
+	var inputEmail = document.querySelector('[name=email]');
+	var textareaText = document.querySelector('[name=text]');
+
+	function modalShow(evt) {
+		evt.preventDefault();
+		modalContact.classList.add('modal-show');
+		modalOverlay.classList.add('overlay-show');
+	}
+
+	function validateForm(evt) {
+		if(!inputName.value || !inputEmail.value || !textareaText.value) {
+			evt.preventDefault();
+
+			modalContact.classList.remove('modal-error');
+			modalContact.offsetWidth = modalContact.offsetWidth;
+			modalContact.classList.add('modal-error');
+
+			if(!inputName.value) {
+				inputName.classList.add('input-invalid');
+			}
+			if(!inputEmail.value) {
+				inputEmail.classList.add('input-invalid');
+			}
+			if(!textareaText.value) {
+				textareaText.classList.add('input-invalid');
+			}			
+		}
+	}
+
+	function validateInput(evt) {
+		if(this.value) {
+			if(this.classList.contains('input-invalid')) {
+				this.classList.remove('input-invalid')
+			}
+		}
+	}
+
+	function modalHide(evt) {
+		evt.preventDefault();
+		modalContact.classList.remove('modal-show');
+		modalOverlay.classList.remove('overlay-show');
+		modalContact.classList.remove('modal-error');
+
+		inputName.classList.remove('input-invalid');
+		inputEmail.classList.remove('input-invalid');
+		textareaText.classList.remove('input-invalid');
+	}
+
+	contactUsLink.addEventListener('click', modalShow);
+
+	inputName.addEventListener('blur', validateInput);
+	inputEmail.addEventListener('blur', validateInput);
+	textareaText.addEventListener('blur', validateInput);
+
+	form.addEventListener('submit', validateForm)
+
+	modalOverlay.addEventListener('click', modalHide);
+	modalClose.addEventListener('click', modalHide);
+
 }
 
 toggleModal();
 
-var contactUsLink = document.querySelector('.contact-us-link');
-var modalContact = document.querySelector('.modal-contact');
-var modalOverlay = document.querySelector('.modal-overlay');
-var modalClose = document.querySelector('.modal-close');
-
-var form = document.querySelector('.contact-form');
-var name = document.querySelector('[name=name]');
-var email = document.querySelector('[name=email]');
-var text = document.querySelector('[name=text]');
-
-function modalHide(evt) {
-	evt.preventDefault();
-	modalContact.classList.remove('modal-show');
-	modalOverlay.classList.remove('overlay-show');
-}
-
-function validateForm(evt) {
-	if(!name.value || !email.value || !text.value) {
-		evt.preventDefault();
-		if(!name.value) {
-			
-		}
-	}
-}
-
-function modalShow(evt) {
-	evt.preventDefault();
-	modalContact.classList.add('modal-show');
-	modalOverlay.classList.add('overlay-show');
-}
-
-contactUsLink.addEventListener('click', modalShow);
-
-form.addEventListener('submit', validateForm)
-
-modalOverlay.addEventListener('click', modalHide);
-modalClose.addEventListener('click', modalHide);
